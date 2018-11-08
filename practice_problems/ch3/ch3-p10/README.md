6d7218513ba4a58be408c616a60e6644633fc700
simomo <qihangzp@gmail.com>
2018-11-08 21:47:56 +0800

    Add readme for ch3 p10

---
+++ practice_problems/ch3/ch3-p10/README.md
@@ -0,0 +1,7 @@
+# Not sure if the reference is used rightly
+
+`ifstream& OpenFile(ifstream&);` It's a little wired...
+
+# It seems that `f.clear()` is not necessariy
+
+Each time `f.open` a new file name, its status will be reset
--- practice_problems/ch3/ch3-p10/main.cpp
+++ practice_problems/ch3/ch3-p10/main.cpp
@@ -1,3 +1,10 @@
+/* 
+Write a function OpenFile that accepts as input an ifstream by reference and prompts the user
+for the name of a file. If the file can be found, OpenFile should return with the ifstream opened
+to read that file. Otherwise, OpenFile should print an error message and reprompt the user.
+(Hint: If you try to open a nonexistent file with an ifstream , the stream goes into a fail state and you
+will need to use .clear() to restore it before trying again).
+*/
 #include <fstream>
 #include <iostream>
 
