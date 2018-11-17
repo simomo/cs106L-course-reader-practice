cb5da039bab529daa4cd7ec16685d326a059c4f3
simomo <qihangzp@gmail.com>
2018-11-17 22:05:29 +0800

    ch4 p12 d)

--- practice_problems/ch4/ch4-p12/main.cpp
+++ practice_problems/ch4/ch4-p12/main.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <cmath>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -11,7 +12,24 @@ enum LengthUnit {
     eLengthUnit_ERROR,
 };
 
-LengthUnit SuffixStringToLengthUnit(string suffix) {
+struct Length {
+    double value;
+    LengthUnit unit;
+};
+
+static LengthUnit SuffixStringToLengthUnit(string suffix);
+static Length ReadLength(void);
+static inline void PrintLength(Length length);
+
+
+int main() {
+    cout << SuffixStringToLengthUnit("aaaaa") << endl;
+
+    PrintLength(ReadLength());
+    return 0;
+}
+
+static LengthUnit SuffixStringToLengthUnit(string suffix) {
     #define DEFINE_UNIT(unit_name, value, symbol, system) if (suffix == #symbol) return eLengthUnit_##symbol;
     #include "units.h"
     #undef DEFINE_UNIT
@@ -19,8 +37,36 @@ LengthUnit SuffixStringToLengthUnit(string suffix) {
     return eLengthUnit_ERROR;
 }
 
-int main() {
-    cout << SuffixStringToLengthUnit("aaaaa") << endl;
-    return 0;
+static Length ReadLength() {
+    cout << "Please enter a mount, like '5 cm': " << endl;
+
+    string userInput;
+    while(getline(cin, userInput)) {
+        stringstream converter;
+        double value;
+        string suffix;
+        string rest;
+        LengthUnit unit;
+        Length length;
+        string errorMsg = "the input format is invalid, it must be like '5 cm'";
+
+        converter << userInput;
+        if ((converter >> value >> suffix) && ! (converter >> rest)) {
+            // cout << "Good! value: " << value << ", suffix: " << suffix << endl;
+            
+            if ((unit = SuffixStringToLengthUnit(suffix)) != eLengthUnit_ERROR) {
+                length.value = value;
+                length.unit = unit;
+                return length;
+            } else {
+                errorMsg = "the suffix is invalid";
+            }
+        }
+
+        cout << "Please check your input, beacuse " << errorMsg << endl;
+    }
 }
 
+static inline void PrintLength(Length length) {
+    cout << "Length[value=" << length.value << ", unit=" << length.unit << "]" << endl;
+}
