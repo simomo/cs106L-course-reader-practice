6933bfcec17edd486d5e232053518a078e039371
simomo <qihangzp@gmail.com>
2018-11-29 23:19:15 +0800

    working on snake

--- self-entertaining/ch5/the_snake/main.cpp
+++ self-entertaining/ch5/the_snake/main.cpp
@@ -1,20 +1,52 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 
 using namespace std;
 
-const string mapFileName = "sample_map.txt";
+const int RetryLimit = 3;
 
-vector<string> LoadWorldMap(string fileName);
+struct GameT {
+    vector<string> gameMap;
+
+};
+
+static GetMapFileName(int retryLimit, ifstream& mapFile);
+static vector<string> LoadWorldMap(ifstream& mapFile);
 
 int main() {
-    vector<string> worldMap = LoadWorldMap(mapFileName);
+    ifstream mapFile;
+    GetMapFileName(RetryLimit, mapFile);
+    vector<string> worldMap = LoadWorldMap(mapFile);
 }
 
-vector<string> LoadWorldMap(string fileName) {
+static GetMapFileName(int retryLimit, ifstream& mapFile) {
+    string fileName;
+    cout << "Please enter the name of map file: " << endl;
+
+    while (retryLimit) {
+        if (!getline(cin, fileName)) {
+            cout << "You just entered an invalid file name:" << fileName << endl;
+            retryLimit--;
+            continue;
+        }
+
+        mapFile.open(fileName);
+        if (!mapFile.is_open()) {
+            cout << "Open file " << fileName << " failed, please try again" << endl;
+            retryLimit--;
+            mapFile.clear();  // ch3-p10 readme: It seems that `f.clear()` is not necessariy
+            continue;
+        }
+        cout << "Open file " << fileName << " successfully!" << endl;
+        break;
+    }
+
+}
+
+static vector<string> LoadWorldMap(ifstream& mapFile) {
     vector<string> worldMap;
 
-
-    return worldMap;    
+    return worldMap;
 }
