b1c5f859c0a27e51277c058999bb714030544529
simomo <qihangzp@gmail.com>
2019-02-26 23:53:28 +0800

    start working on practice ch5 7th "deque to ring"

---
+++ practice_problems/ch5/7-deque-to-ring/main.cpp
@@ -0,0 +1,13 @@
+#include <deque>
+
+using namespace std;
+
+struct Ring {
+    deque<int> _ring;
+
+    // copy constructor & Ring(deque<int> ) constructor
+
+    // Read & Write
+
+    // CursorClockwise & CursorCounterClockwise
+}
