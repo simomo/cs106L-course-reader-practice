6f7332df8c9723ed607c7c714e05d67782cb6538
simomo <qihangzp@gmail.com>
2019-02-28 01:40:23 +0800

    default & deque<int> contructor & call deque init & class init

--- practice_problems/ch5/7-deque-to-ring/main.cpp
+++ practice_problems/ch5/7-deque-to-ring/main.cpp
@@ -3,11 +3,20 @@
 using namespace std;
 
 struct Ring {
-    deque<int> _ring;
+    deque<int> m_ring;
 
     // copy constructor & Ring(deque<int> ) constructor
+    Ring() {}
+    Ring(deque<int> ring): m_ring(ring) {}
 
     // Read & Write
 
     // CursorClockwise & CursorCounterClockwise
+};
+
+int main() {
+    deque<int> init {1, 2, 3, 4, 5};
+    Ring ring {init};
+
+    return 0;
 }
