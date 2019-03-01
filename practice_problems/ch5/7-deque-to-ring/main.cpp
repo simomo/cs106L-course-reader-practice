193ab355f60e0983b77d36095a61123ce66da74f
simomo <qihangzp@gmail.com>
2019-03-02 00:00:00 +0800

    Learn deque and vector constructors, then build similar ones for `struct Ring`

--- practice_problems/ch5/7-deque-to-ring/main.cpp
+++ practice_problems/ch5/7-deque-to-ring/main.cpp
@@ -7,7 +7,9 @@ struct Ring {
 
     // copy constructor & Ring(deque<int> ) constructor
     Ring() {}
+    Ring(initializer_list<int> _l): m_ring(_l) {}
     Ring(deque<int> ring): m_ring(ring) {}
+    Ring(Ring& _ring): m_ring(_ring.m_ring) {}
 
     // Read & Write
 
@@ -17,6 +19,9 @@ struct Ring {
 int main() {
     deque<int> init({1, 2, 3, 4, 5});
     Ring ring(init);
+    Ring newRing(ring);
+
+    Ring super({1, 2, 3, 4, 5});
 
     return 0;
 }
