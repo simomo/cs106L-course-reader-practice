30d15b93add359fbb6b6175dd9bbf7055edea3a9
simomo <qihangzp@gmail.com>
2019-03-03 00:20:48 +0800

    Read/Write/CursorClockwise/CursorCounterClockwise and chain API

--- practice_problems/ch5/7-deque-to-ring/main.cpp
+++ practice_problems/ch5/7-deque-to-ring/main.cpp
@@ -1,4 +1,6 @@
 #include <deque>
+#include <sstream>
+#include <iostream>
 
 using namespace std;
 
@@ -12,8 +14,38 @@ struct Ring {
     Ring(Ring& _ring): m_ring(_ring.m_ring) {}
 
     // Read & Write
+    int Read() {
+        return m_ring[0];
+    }
+
+    void Write(int i) {
+        m_ring[0] = i;
+    }
 
     // CursorClockwise & CursorCounterClockwise
+    Ring* CursorClockwise() {
+        m_ring.push_front(m_ring.back());
+        m_ring.pop_back();
+        return this;
+    }
+
+    Ring* CursorCounterClockwise() {
+        m_ring.push_back(m_ring[0]);
+        m_ring.pop_front();
+        return this;
+    }
+
+    // toString
+    string toString() {
+        stringstream fmt;
+        fmt << "[deque<int>:";
+        for (deque<int>::iterator i = m_ring.begin(); i != m_ring.end(); ++i) {
+            fmt << " " << *i;
+        }
+        fmt << "]" << endl;
+
+        return fmt.str();
+    }
 };
 
 int main() {
@@ -22,6 +54,10 @@ int main() {
     Ring newRing(ring);
 
     Ring super({1, 2, 3, 4, 5});
+
+    cout << ring.toString() << endl;
+    cout << ring.CursorClockwise()->toString() << endl;
+    cout << ring.CursorCounterClockwise()->toString() << endl;
 
     return 0;
 }
