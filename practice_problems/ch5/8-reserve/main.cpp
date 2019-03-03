ab3c847649dafc1aa869361e78dd448ef0ca1472
simomo <qihangzp@gmail.com>
2019-03-03 23:34:18 +0800

    Start ch5 8-reserve

---
+++ practice_problems/ch5/8-reserve/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ Fill this table:
+ Data structure     Reserve or not      front or back       Time Costs
+ --------------     --------------      -------------       ----------
+ dequeue            Yes                 front
+ dequeue            No                  front
+ dequeue            Yes                 back
+ dequeue            No                  back
+ vector             Yes                 front
+ vector             No                  front
+ vector             Yes                 back
+ vector             No                  back
+ */
+
+#include <deque>
+#include <vector>
+#include <iostream>
+
+int main() {return 0;}
