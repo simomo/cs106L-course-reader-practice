c34dc257308c31ea57381c98baab0d7dad61eb83
simomo <qihangzp@gmail.com>
2019-03-17 23:28:12 +0800

    Building the skeleton for assignment 1.

--- assignments/01_GraphViz/src/main.cpp
+++ assignments/01_GraphViz/src/main.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
+#include <string>
 #include "SimpleGraph.h"
 
 using std::cout;	using std::endl;
+using std::string;
 
 void Welcome();
+void UserInputName(string& graphName);
+void LoadGraphFile(string graphName, SimpleGraph& graph);
 
 // Main method
 int main() {
     Welcome();
+    SimpleGraph graph;
+    string graphName;
+
+    UserInputName(graphName);
+    LoadGraphFile("10clique", graph);
+    InitGraphVisualizer(graph);
+    DrawGraph(graph);
+    //TODO: Iteration & time elapse
     return 0;
 }
+
+void UserInputName(string& graphName) {}
+
+void LoadGraphFile(string graphName, SimpleGraph& graph) {}
 
 /* Prints a message to the console welcoming the user and
  * describing the program. */
