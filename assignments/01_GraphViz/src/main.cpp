eeeed4f32edee115af4d923d2550f9a128de83ae
simomo <qihangzp@gmail.com>
2019-03-19 00:05:00 +0800

    Working on Assignment 1: UserInputName

--- assignments/01_GraphViz/src/main.cpp
+++ assignments/01_GraphViz/src/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "SimpleGraph.h"
 
-using std::cout;	using std::endl;
+using std::cout;
+using std::cin;
+using std::endl;
 using std::string;
+using std::ifstream;
 
 void Welcome();
-void UserInputName(string& graphName);
+void UserInputName(string& graphName, ifstream& graphFile);
 void LoadGraphFile(string graphName, SimpleGraph& graph);
 
 // Main method
@@ -14,8 +18,9 @@ int main() {
     Welcome();
     SimpleGraph graph;
     string graphName;
+    ifstream graphFile;
 
-    UserInputName(graphName);
+    UserInputName(graphName, graphFile);
     LoadGraphFile("10clique", graph);
     InitGraphVisualizer(graph);
     DrawGraph(graph);
@@ -23,7 +28,24 @@ int main() {
     return 0;
 }
 
-void UserInputName(string& graphName) {}
+void UserInputName(string& graphName, ifstream& graphFile) {
+    while (true) {
+        cout << "Please enter the graph file name: ";
+        if (!getline(cin, graphName)) {
+            cout << endl << "Cant get what you typed, please try again" << endl;
+            continue;
+        }
+
+        graphFile.open(graphName);
+        if (graphFile.is_open()) {
+            break;
+        }
+
+        cout << endl << "Cant open the file " << graphName << endl;
+    }
+
+
+}
 
 void LoadGraphFile(string graphName, SimpleGraph& graph) {}
 
