e9624c808eab90bd52c8be25cb0c7679f22e4a28
simomo <qihangzp@gmail.com>
2019-03-20 00:07:41 +0800

    Working on the assignment 1: LoadGraphFile
    Why stringstream >> aInt >> anotherInt not work?

--- assignments/01_GraphViz/src/main.cpp
+++ assignments/01_GraphViz/src/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <cmath>
 #include "SimpleGraph.h"
+
+#define RAND_MAX 200
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::ifstream;
+using std::stringstream;
 
 void Welcome();
 void UserInputName(string& graphName, ifstream& graphFile);
-void LoadGraphFile(string graphName, SimpleGraph& graph);
+void LoadGraphFile(ifstream& graphFile, SimpleGraph& graph);
 
 // Main method
 int main() {
@@ -21,7 +26,7 @@ int main() {
     ifstream graphFile;
 
     UserInputName(graphName, graphFile);
-    LoadGraphFile("10clique", graph);
+    LoadGraphFile(graphFile, graph);
     InitGraphVisualizer(graph);
     DrawGraph(graph);
     //TODO: Iteration & time elapse
@@ -47,7 +52,33 @@ void UserInputName(string& graphName, ifstream& graphFile) {
 
 }
 
-void LoadGraphFile(string graphName, SimpleGraph& graph) {}
+void inline randNodePos(Node& node) {
+    node.x = rand() % RAND_MAX;
+    node.y = rand() % RAND_MAX;
+}
+
+void LoadGraphFile(ifstream& graphFile, SimpleGraph& graph) {
+    stringstream converter;
+    string nodesNumStr;
+    int nodesNum;
+
+    string oneLine;
+
+    if (!getline(graphFile, nodesNumStr)) {return;}
+    converter << nodesNumStr;
+    converter >> nodesNum;
+    graph.nodes.resize(nodesNum);
+
+    while (getline(graphFile, oneLine)) {
+        Edge edge;
+        converter << oneLine;
+        converter >> edge.start >> edge.end;  // end=8 start=578550992 why?!
+        graph.edges.push_back(edge);
+
+        randNodePos(graph.nodes[edge.start]);
+        randNodePos(graph.nodes[edge.end]);
+    }
+}
 
 /* Prints a message to the console welcoming the user and
  * describing the program. */
