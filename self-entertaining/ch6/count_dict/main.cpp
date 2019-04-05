429f7bc3f0f6cc086860f6005f9df90582b8dc4e
simomo <qihangzp@gmail.com>
2019-04-05 22:37:17 +0800

    Working on the count dict

--- self-entertaining/ch6/count_dict/main.cpp
+++ self-entertaining/ch6/count_dict/main.cpp
@@ -1,3 +1,33 @@
+#include <fstream>
+#include <string>
+#include <map>
+#include <iostream>
+
+
+using namespace std;
+
+bool initCountMap(map<string, int>& countMap) {
+    return true;
+}
+
 int main() {
+    // Read keywords.txt, init the keywords map
+    map<string, int> countMap;
+    if (!initCountMap(countMap)) {
+        return -1;
+    }
+
+    // Open current file
+    string fileName = __FILE__;
+    ifstream currentFile(fileName);
+    if (!currentFile.is_open()) {
+        return -2;
+    }
+
+    // Iter each file, fill the keywords map
+    string oneLine;
+    while (getline(currentFile, oneLine)) {
+        cout << oneLine << endl;
+    }
     return 0;
 }
