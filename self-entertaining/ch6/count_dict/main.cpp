0369fb3fc94fc4e068ff95703b89ecd4d13e2902
simomo <qihangzp@gmail.com>
2019-04-07 23:32:08 +0800

    count map is done

--- self-entertaining/ch6/count_dict/main.cpp
+++ self-entertaining/ch6/count_dict/main.cpp
@@ -2,11 +2,21 @@
 #include <string>
 #include <map>
 #include <iostream>
+#include <sstream>
 
 
 using namespace std;
 
 bool initCountMap(map<string, int>& countMap) {
+    ifstream kw("keywords.txt");
+    if (!kw.is_open()) {
+        return false;
+    }
+
+    string oneLine;
+    while (getline(kw, oneLine)) {
+        countMap[oneLine] = 0;
+    }
     return true;
 }
 
@@ -26,8 +36,32 @@ int main() {
 
     // Iter each file, fill the keywords map
     string oneLine;
+    stringstream ss;
     while (getline(currentFile, oneLine)) {
-        cout << oneLine << endl;
+        // while(true) => while true 
+        for (int i = 0; i < oneLine.size(); ++i) {
+            if (ispunct(oneLine[i]) && oneLine[i] != '_') {
+                oneLine[i] = ' ';
+            }
+        }
+        ss << oneLine;
+        
+        string oneWord;
+        while (ss >> oneWord) {
+            if (countMap.count(oneWord)) {
+                countMap[oneWord] += 1;
+            }
+        }
+
+        ss.clear();
+        ss.str("");
+    }
+
+    // Iter the keywords map, print the non-zero pairs
+    for (map<string, int>::iterator i = countMap.begin(); i != countMap.end(); ++i) {
+        if (i->second > 0) {
+            cout << i-> first << " " << i->second << endl;
+        }
     }
     return 0;
 }
