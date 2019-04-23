fbf5471bbb230282de07b904a04e7399bf3c6604
simomo <qihangzp@gmail.com>
2019-04-23 23:12:45 +0800

    Finished the ch8 8-shopping-list

--- practice_problems/ch8/8-shoping-list/grocery.h
+++ practice_problems/ch8/8-shoping-list/grocery.h
@@ -2,6 +2,7 @@
 #define GROCERY_H
 #include <string>
 #include <map>
+#include <cassert>
 
 using namespace std;
 
@@ -11,7 +12,7 @@ public:
     void addItem(string quantity, string item);
     void removeItem(string item);
     string itemQuantity(string item);
-    bool itemExists(string item);public:
+    bool itemExists(string item);
 private:
     map<string, string> theList;
 };
--- practice_problems/ch8/8-shoping-list/main.cpp
+++ practice_problems/ch8/8-shoping-list/main.cpp
@@ -1,8 +1,20 @@
 #include "grocery.h"
 
 void GroceryList::addItem(string q, string i) {
+    theList[q] = i;
+}
 
+void GroceryList::removeItem(string item) {
+    theList.erase(item);
+}
 
+string GroceryList::itemQuantity(string item) {
+    assert(theList.find(item) != theList.end());
+    return theList[item];
+}
+
+bool GroceryList::itemExists(string item) {
+    return theList.find(item) != theList.end();
 }
 
 int main() {
