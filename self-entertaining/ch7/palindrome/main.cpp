3fc1233dfde7bd2fa504867f075c52a631315cff
simomo <qihangzp@gmail.com>
2019-07-11 23:34:26 +0800

    Working on ch7 pelindrome

--- self-entertaining/ch7/palindrome/main.cpp
+++ self-entertaining/ch7/palindrome/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <cassert>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,7 +13,28 @@ bool IsPalindrome(string input) {
     return true;
 }
 
+bool IsPalindrome1(string input) {
+    string reversed = input;
+    reverse(reversed.begin(), reversed.end());
+    return reversed == input;
+}
+
+bool IsPalindrome2(string input) {
+    return equal(input.begin(), input.begin() + input.size()/2, input.rbegin());
+}
+
+// 3
+bool IsNotAlpha(char c) {
+    return !isalpha(c);
+}
+bool IsPalindrome3(string input) {
+    // The remove_if & erase pattern: https://stackoverflow.com/a/42116415
+    input.erase(remove_if(input.begin(), input.end(), IsNotAlpha), input.end());
+}
+
 int main() {
     assert(IsPalindrome("abcba"));
+    assert(IsPalindrome1("abcba"));
+    assert(IsPalindrome2("abcba"));
     return 0;
 }
