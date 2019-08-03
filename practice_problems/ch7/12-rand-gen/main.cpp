1041f717f121a5cf59e002a4ee74a1bb9798eb6a
simomo <qihangzp@gmail.com>
2019-08-04 00:51:47 +0800

    ch7 12

---
+++ practice_problems/ch7/12-rand-gen/main.cpp
@@ -0,0 +1,7 @@
+// 12. Using the generate_n algorithm, the rand function, and a back_insert_iterator , show how to
+// populate a vector with a specified number of random values. Then use accumulate to compute
+// the average of the range.
+
+int main() {
+    return 0;
+}
