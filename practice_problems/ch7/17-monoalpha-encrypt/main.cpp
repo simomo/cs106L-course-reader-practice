069e40a2a32c71c75c6d7223f5f2915cdd48da6e
simomo <qihangzp@gmail.com>
2019-09-05 09:06:54 +0800

    ch7 17

--- practice_problems/ch7/17-monoalpha-encrypt/main.cpp
+++ practice_problems/ch7/17-monoalpha-encrypt/main.cpp
@@ -8,7 +8,17 @@ int main() {
     string alphabet = "abcdefghijklmnopqrstuvwxyz";
     string cipher = alphabet;
     random_shuffle(cipher.begin(), cipher.end());
+
+    string message = "HiIloveYou";
+    string encryptedMsg(message);
+    transform(message.begin(), message.end(), encryptedMsg.begin(),
+        [](char c){
+            return tolower(c);
+        });
+
     cout << cipher << endl;
+    cout << message << endl;
+    cout << encryptedMsg << endl;
 
     return 0;
 }
